f1ca9d4a3ad1a03b172c0ec8335c91236ba21980
Gökalp Avcu <72872909+GokalpAvcu@users.noreply.github.com>
2021-05-05 12:03:36 +0300

    Add files via upload

---
+++ numbers.js
@@ -0,0 +1,30 @@
+// Numbers
+
+let val;
+
+// val= Number('10');
+// val = parseInt("10");
+// val = parseFloat("10.5");
+// val = isNan("10");
+// val = isNan("a10");
+
+// var num= 10.123456789;
+// val = num.toPrecision(4);
+// val = num.toFixed(4);
+
+val = Math.PI;
+val = Math.round(2.4);
+val = Math.round(2.7);
+val = Math.ceil(2.4);
+val = Math.ceil(2.6);
+val = Math.floor(2.4);
+val = Math.floor(2.7);
+val = Math.sqrt(64);
+val = Math.pow(2,4);
+val = Math.abs(-100);
+val = Math.min(1,2,3,2,4,9);
+val = Math.max(4,5,6,4,98,7);
+val = Math.floor(Math.random()*100+1);
+
+console.log(val);
+console.log(typeof val);
