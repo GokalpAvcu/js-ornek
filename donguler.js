68cfcfbc0cef04ce2d9ee7f29c76ce3b467b47e7
Gökalp Avcu <72872909+GokalpAvcu@users.noreply.github.com>
2021-05-25 17:20:53 +0300

    Add files via upload

---
+++ donguler.js
@@ -0,0 +1,16 @@
+// Loops
+
+// for loop
+
+for(let i =1; i<=10; i++){
+   
+  if(i==3){
+      console.log("en sevdiğim rakam :"+i);
+      continue; // döngü burda durur kendini tekrar etmez
+  }
+  console.log(i);
+
+
+
+
+}
