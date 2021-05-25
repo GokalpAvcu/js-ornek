9a3620314dd137f420bb0d7fdc27fda534a8f05f
Gökalp Avcu <72872909+GokalpAvcu@users.noreply.github.com>
2021-05-25 17:24:06 +0300

    Add files via upload

--- donguler.js
+++ donguler.js
@@ -2,18 +2,19 @@
 
 // for loop
 
-for(let i =1; i<=10; i++){
+// for(let i =1; i<=10; i++){
    
-  if(i==3){
-      console.log("en sevdiğim rakam :"+i);
-      continue; // döngü burda durur kendini tekrar etmez
-  }
-  if (i==6){
-      console.log("en sevmediğim rakam:6");
-      break; //6'dan sonrası devam etmez
-  }
+//   if(i==3){
+//       console.log("en sevdiğim rakam :"+i);
+//       continue; // döngü burda durur kendini tekrar etmez
+//   }
+//   if (i==6){
+//       console.log("en sevmediğim rakam:6");
+//       break; //6'dan sonrası devam etmez. for döngüsü break'i gördüğü an sonlanır.
+//   }
 
-  console.log(i);
+//   console.log(i);
 
 
-}
+// }
+
