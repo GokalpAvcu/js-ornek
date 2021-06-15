53b785a6d97a5792039bb7389699380e28e4d5b1
Gökalp Avcu <72872909+GokalpAvcu@users.noreply.github.com>
2021-06-15 23:53:42 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,30 @@
+// Functions
+
+function yasHesapla(dogumYili){
+    return 2018 - dogumYili;
+}
+
+
+let ageAda = yasHesapla(2012);
+let ageYigit = yasHesapla(2010);
+let ageCinar = yasHesapla(2017);
+
+console.log(ageAda);
+console.log(ageYiğit);
+console.log(ageCinar);
+
+
+function EmekligeKacYilKaldi(dogumYili,isim){
+    let yas = yasHesapla(dogumYili);
+    let emeklilik = 65 - yas;
+
+    if(emeklilik>0){
+        console.log(`Emekli olmanıza ${emeklilik} yıl kaldı`);
+    }else{
+        console.log("Zaten emekli oldunuz");
+    }
+}
+
+EmekligeKacYilKaldi(2012,"Ada");
+EmekligeKacYilKaldi(2010,"Yiğit");
+EmekligeKacYilKaldi(2012,"Ada");
