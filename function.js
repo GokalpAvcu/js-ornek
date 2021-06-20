557e11aab7a689570545bbd4e1c7ec29158958b9
Gökalp Avcu <72872909+GokalpAvcu@users.noreply.github.com>
2021-06-20 15:30:05 +0300

    Add files via upload

--- function.js
+++ function.js
@@ -10,7 +10,7 @@ let ageYigit = yasHesapla(2010);
 let ageCinar = yasHesapla(2017);
 
 console.log(ageAda);
-console.log(ageYiğit);
+console.log(ageYigit);
 console.log(ageCinar);
 
 
@@ -19,7 +19,7 @@ function EmekligeKacYilKaldi(dogumYili,isim){
     let emeklilik = 65 - yas;
 
     if(emeklilik>0){
-        console.log(`Emekli olmanıza ${emeklilik} yıl kaldı`);
+        console.log(`${isim} Emekli olmana ${emeklilik} yıl kaldı`);
     }else{
         console.log("Zaten emekli oldunuz");
     }
@@ -28,3 +28,45 @@ function EmekligeKacYilKaldi(dogumYili,isim){
 EmekligeKacYilKaldi(2012,"Ada");
 EmekligeKacYilKaldi(2010,"Yiğit");
 EmekligeKacYilKaldi(2012,"Ada");
+
+
+// Function Declarations
+
+// function sum(a,b){
+//     var c = a+b;
+//     return c;
+// }
+
+// console.log(sum(10,20));
+
+// Function Expressions
+
+// const sum = function(a,b){
+
+//     if(typeof a === 'undefined'){ a=0; }
+//     if(typeof b === 'undefined'){ b=0; }
+
+//     var c = a+b;
+//     return c;
+// }
+
+// ES6 Default Parameters
+const sum = function(a=0,b=0){
+    var c = a+b;
+    return c;
+}
+
+function sumAll(){
+    var total = 0;
+    for(let i =0;i<arguments.length;i++){
+         total+=arguments[i];   
+    }
+    return total;
+}
+
+console.log(sum(10,30));
+console.log(sum(10)); // NaN
+console.log(sum(10,30,40,50));
+console.log(sumAll(10,20,30,10,30));
+
+
