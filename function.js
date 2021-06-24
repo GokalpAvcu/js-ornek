80afbd5e74a69a6a83d2601d779ab5c470b595fe
Gökalp Avcu <72872909+GokalpAvcu@users.noreply.github.com>
2021-06-24 14:15:26 +0300

    Add files via upload

--- function.js
+++ function.js
@@ -103,13 +103,23 @@ if(hesap.bakiye >= miktar){
     if(toplam>=miktar){
         if(confirm("ek hesabınızı kullanmak ister misiniz")){
             console.log("paranızı alabilirsiniz");
+            var bakiye = hesap.bakiye;
+            var ekhesap = miktar - bakiye;
+            hesap.bakiye = 0;
+            hesap.ekhesap = hesap.ekhesap - ekhesap;
+        }else{
+            console.log(`${hesap.hesapNo} nolu hesabınızda ${miktar} TL bulunmamaktadır.`);
         }
+    }else{
+        console.log('üzgünüz bakiye yetersiz');
     }
 }
 
 }
 
+
 paraCek(hesapA,2000);
+paraCek(hesapA,1100);
 // paraCek(hesapB,2000);
 
 
