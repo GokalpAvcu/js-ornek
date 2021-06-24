d6da8f5b62a26cd03891236cfad7d2b74c17062f
Gökalp Avcu <72872909+GokalpAvcu@users.noreply.github.com>
2021-06-24 15:40:23 +0300

    Add files via upload

---
+++ window.js
@@ -0,0 +1,40 @@
+// Window Object
+
+let val;
+var a = 10;
+function abc(){
+    return 0;
+};
+
+val= window;
+
+
+// alert
+// alert("Merhaba");   // ekrana merhaba yazar
+
+
+// prompt
+// var val = prompt("bir sayı giriniz");
+
+
+// confirm
+// val = confirm("Emin misiniz ?");
+
+// if(val){
+//     console.log("ok");
+// }else{
+//     console.log("no");
+// }
+
+
+// scroll
+// val = window.scrollX;  // scroll barları ayarlar
+// val = window.scrollY;
+
+// location
+
+val = window.location;
+
+
+
+console.log(val);
