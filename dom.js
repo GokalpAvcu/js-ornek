9aeba0edcc00f1094c5e9c103329d1d8b38b7910
Gökalp Avcu <72872909+GokalpAvcu@users.noreply.github.com>
2021-06-28 20:58:29 +0300

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,54 @@
+// let val;
+
+// val = window.document;
+// val = document.all;
+// val = document.all.length;
+// val = document.all[8];
+// val = document.head;
+// val = document.body;
+// val = document.anchors;
+// val = document.URL;
+// val = document.domain;
+// val = document.images;
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
+// val = document.forms[0].action;
+// val = document.scripts;
+// val = document.scripts[2];
+// val = document.scripts[2].getAttribute('src');
+
+
+// console.log(val);
+
+//---------------------------------------------------------------------------------------------------------
+
+// Selecting Elements
+
+// *** Single Elements
+ let val;
+
+ val = document.getElementById('header');
+ val = document.getElementById('header').id;
+ val = document.getElementById('header').className;
+
+ val = document.getElementById('header');
+ // val = val.id;
+ // val = val.className;
+
+ val.style.fontSize="45px";
+ val.style.color="red";
+ val.style.fontWeight="bold";
+
+ // val.style.display="none";
+
+ val = document.getElementById("header").innerText="my ToDo App";
+
+
+
+ console.log(val);
+
+
+// *** Multiple Elements
+
