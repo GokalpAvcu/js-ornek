9c5ce721bf3077e300a70833b3552636feb7357d
Gökalp Avcu <72872909+GokalpAvcu@users.noreply.github.com>
2021-06-28 22:14:35 +0300

    Add files via upload

--- dom.js
+++ dom.js
@@ -56,21 +56,78 @@
 
 // document.querySelector()
 
-console.log(document.querySelector("#header"));
-console.log(document.querySelector('.card-header'));
-console.log(document.querySelector('div'));
+// console.log(document.querySelector("#header"));
+// console.log(document.querySelector('.card-header'));
+// console.log(document.querySelector('div'));
 
-document.querySelector('li').style.color='red';
-document.querySelector('li:last-child').style.color='blue';
-document.querySelector('li:nth-child(2)').style.color='yellow';
-document.querySelector('li:nth-child(3)').textContent='task item';
-document.querySelector('li:nth-child(4)').textContent='task item';
+// document.querySelector('li').style.color='red';
+// document.querySelector('li:last-child').style.color='blue';
+// document.querySelector('li:nth-child(2)').style.color='yellow';
+// document.querySelector('li:nth-child(3)').textContent='task item';
+// document.querySelector('li:nth-child(4)').textContent='task item';
 
-// document.querySelector('li').className='list-group-item list-group-item-primary';
+// // document.querySelector('li').className='list-group-item list-group-item-primary';
 
-document.querySelector('li').classList.add('active');
+// document.querySelector('li').classList.add('active');
 
-
+//------------------------------------------------------------------------------------------------------------------
 
 // *** Multiple Elements
+
+// Selecting Elements
+
+// *** Single Elements
+//    *** document.getElementById()
+//    *** document.querySelector()
+
+// *** Multiple Elements
+
+// class name
+// document.getElementsByClassName()
+
+let val;
+
+//val = document.getElementsByClassName('list-group-item');
+// val = document.getElementsByClassName('list-group-item')[0];
+
+// val = document.getElementsByClassName('list-group-item')[2];
+
+// val = val[2];
+
+// val[1].style.color='blue';
+// val[1].style.fontSize='25px';
+// val[2].textContent='new item';
+
+// for(let i=0; i<val.length;i++){
+//     console.log(val[i].style.color='red');
+//     console.log(val[i].textContent='new item');
+// }
+
+
+// document.getElementsByTagName()
+
+// val = document.getElementsByTagName('li');
+// val = document.getElementById('task-list').getElementsByTagName('a');
+
+
+// document.querySelectorAll()
+
+// val = document.querySelectorAll('li');
+
+// val.forEach(function(item,index){
+//     item.textContent= `${index} - hello`;
+// });
+
+val = document.querySelectorAll('li:nth-child(even)');
+
+val.forEach(function(item){
+   item.style.background='#ccc';
+});
+
+console.log(val);
+
+
+
+
+
 
